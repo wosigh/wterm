8c9027b09b19a40c04b1de0868bb0a6451579fc7
Ryan Hope <rmh3093@gmail.com>
2011-09-27 11:37:03 -0400

    use login -f root instead of sh -l

--- node-service/TTYOpenAssistant.js
+++ node-service/TTYOpenAssistant.js
@@ -8,27 +8,22 @@ TTYOpenAssistant.uniqid = function() {
 
 TTYOpenAssistant.prototype.run = function(future, subscription) {
 
-    //var args = this.controller.args;
-    
-    //tty.setRawMode(true);
-    
-    try {
-		process.chdir(process.env['HOME'])
-	} catch (e) {
-		console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-		console.log([e,process.env['HOME']])
-		console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-	}
-
+    var args = this.controller.args;
+ 
 	var id = TTYOpenAssistant.uniqid();
-	ttys[id] = tty.open('/media/cryptofs/apps/usr/palm/applications/us.ryanhope.wterm/bin/sh', ['-l']);
+	ttys[id] = tty.open('/bin/login',['-f','root']);
 	//tty.setWindowSize(ttys[id][0], 30, 100)
 
     future.result = { tty_id: id };
 
 	ttys[id][0].on('data', function(data) {
+		var i = 0;
 		var s = subscription.get();
-		s.result = { data: data.toString() };
+		while (i+1000<data.length) {
+			s.result = { data: data.toString(start=i,end=i+1000) };
+			i = i + 1000;
+		}
+		s.result = { data: data.toString(start=i) };
 	});
 	
 };
